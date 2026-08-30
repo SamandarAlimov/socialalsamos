195e3d3f6219fd19bc4a6469421e19a3751bbebb
Samandar <74458526+SamandarAlimov@users.noreply.github.com>
2026-08-30 08:29:16 +0500

    refactor(create): route production create to modular composer

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ import SecurityPage from "./pages/SecurityPage";
 import ChatWallpaperPage from "./pages/ChatWallpaperPage";
 import DataStoragePage from "./pages/DataStoragePage";
 import PaymentSettingsPage from "./pages/PaymentSettingsPage";
-import CreatePage from "./pages/CreatePage";
 import ComposePage from "./pages/ComposePage";
 import StickerPacksPage from "./pages/StickerPacksPage";
 import StickerModerationPage from "./pages/StickerModerationPage";
@@ -122,9 +121,10 @@ function AppRoutes() {
         <Route path="/marketplace" element={<MarketplacePage />} />
         <Route path="/map" element={<MapPage />} />
         <Route path="/notifications" element={<NotificationsPage />} />
-        <Route path="/create" element={<CreatePage />} />
-        {/* Yangi modulli post yaratish oqimi (sinov bosqichida) */}
-        <Route path="/compose" element={<ComposePage />} />
+        {/* Create production entry point: modular composer. */}
+        <Route path="/create" element={<ComposePage />} />
+        {/* Eski test URL lar buzilmasin; canonical yo'l /create. */}
+        <Route path="/compose" element={<Navigate to="/create" replace />} />
         {/* Stiker paketlari: o'z paketi va havola orqali kelgan paket.
             Diqqat: "moderation" marshruti ":slug" dan OLDIN turishi shart,
             aks holda slug marshruti uni yutib yuboradi. */}
