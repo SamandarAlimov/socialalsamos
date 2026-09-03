cfea1cdcc3c349abf3b4ca84a257493f69a419c3
Samandar <74458526+SamandarAlimov@users.noreply.github.com>
2026-09-03 08:29:41 +0500

    refactor(ui): use shared stacking policy

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -3,6 +3,7 @@ import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
 
 import { cn } from "@/lib/utils";
+import { UI_LAYER } from "@/lib/uiLayers";
 
 /**
  * Global modal stack:
@@ -11,9 +12,6 @@ import { cn } from "@/lib/utils";
  * AppLayout collapse control z-[1300], xarita/create chrome ham shu diapazonda.
  * Modal backdrop ularning barchasini hira qiladi, content esa backdropdan yuqori.
  */
-const DIALOG_OVERLAY_LAYER = "z-[6000]";
-const DIALOG_CONTENT_LAYER = "z-[6010]";
-
 const Dialog = DialogPrimitive.Root;
 
 const DialogTrigger = DialogPrimitive.Trigger;
@@ -30,7 +28,7 @@ const DialogOverlay = React.forwardRef<
     ref={ref}
     className={cn(
       "fixed inset-0 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-      DIALOG_OVERLAY_LAYER,
+      UI_LAYER.modalOverlay,
       className,
     )}
     {...props}
@@ -48,7 +46,7 @@ const DialogContent = React.forwardRef<
       ref={ref}
       className={cn(
         "fixed left-[50%] top-[50%] grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
-        DIALOG_CONTENT_LAYER,
+        UI_LAYER.modalContent,
         className,
       )}
       {...props}
