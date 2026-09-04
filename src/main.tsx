7b17967a31b58fdddd3d42d3d6dadb4a6ad6c7c2
Samandar <74458526+SamandarAlimov@users.noreply.github.com>
2026-09-04 05:49:22 +0500

    Load Alsamos mobile video watch styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
+import "./styles/video-watch.css";
 import "./i18n";
 
 createRoot(document.getElementById("root")!).render(
