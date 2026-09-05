997756c12718284a6e12e016921f719c282b3653
Samandar <74458526+SamandarAlimov@users.noreply.github.com>
2026-09-05 14:13:51 +0500

    feat(ads): route experiments inside Ads Center

--- src/pages/AdsPage.tsx
+++ src/pages/AdsPage.tsx
@@ -1,5 +1,13 @@
+import { useLocation } from 'react-router-dom';
 import { AdsManagerPage } from '@/components/ads/AdsManagerPage';
+import AdsExperimentsPage from '@/pages/AdsExperimentsPage';
 
 export default function AdsPage() {
+  const location = useLocation();
+
+  if (location.pathname === '/ads/experiments') {
+    return <AdsExperimentsPage />;
+  }
+
   return <AdsManagerPage />;
 }
