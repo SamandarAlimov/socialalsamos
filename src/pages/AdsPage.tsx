1901e079521305cd3ec1bf66cacf36f7a24d0f9e
Samandar <74458526+SamandarAlimov@users.noreply.github.com>
2026-09-05 14:16:49 +0500

    feat(ads): route campaigns to normalized Campaign Studio

--- src/pages/AdsPage.tsx
+++ src/pages/AdsPage.tsx
@@ -1,9 +1,14 @@
 import { useLocation } from 'react-router-dom';
 import { AdsManagerPage } from '@/components/ads/AdsManagerPage';
+import AdsCampaignsV4Page from '@/pages/AdsCampaignsV4Page';
 import AdsExperimentsPage from '@/pages/AdsExperimentsPage';
 
 export default function AdsPage() {
   const location = useLocation();
+
+  if (location.pathname === '/ads/campaigns') {
+    return <AdsCampaignsV4Page />;
+  }
 
   if (location.pathname === '/ads/experiments') {
     return <AdsExperimentsPage />;
